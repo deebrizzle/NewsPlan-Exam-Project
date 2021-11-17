f4c72af963b73b747d1029125aa071bc8942a0a5
emte <emte@itu.dk>
2021-11-17 09:26:01 +0100

    Initialize Parse

--- webserver/src/index.js
+++ webserver/src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Parse from "parse";
+
+Parse.initialize(process.env.REACT_APP_APPLICATION_KEY, process.env.REACT_APP_JS_KEY)
+Parse.serverURL = "https://parseapi.back4app.com/"
 
 ReactDOM.render(
   <React.StrictMode>
