13a4f9e3231ed8141ce03b18d5c9c614cc93cdbb
emte <emte@itu.dk>
2021-11-19 16:47:10 +0100

    Fetching Ideas (Error when fetching the source)

--- webserver/src/components/IdeaTable.js
+++ webserver/src/components/IdeaTable.js
@@ -1,22 +1,48 @@
 import { DataGrid } from '@mui/x-data-grid';
-
-const rows= [
-    { id: 1, col1: '01-04-21', col2: 'KSM', col3: 'Mette F.: Deleted message' , col4: 'The Mink Comission waited for text messages..'},
-    { id: 2, col1: '01-04-21', col2: 'MIH', col3: 'Local election' , col4: 'Do you know who youd like to wote for?' },
-    { id: 3, col1: '01-04-21', col2: 'MIH', col3: 'Climate changes in weather' , col4: 'How the weather in DK changes the climate..' },
-    { id: 4, col1: '01-04-21', col2: 'GTL', col3: 'The premier Leauge status' , col4: 'Live updates, scores, etc...' },
-    { id: 5, col1: '01-04-21', col2: 'KSM', col3: 'COVID-19: what now?' , col4: 'How has Corona affected peoples daily lives..' },
-    { id: 6, col1: '01-04-21', col2: 'GTL', col3: 'Olympic winter games 2022' , col4: 'Oive updates from Bejing, follow schedule, rep..' }
-  ];
+import Parse from "parse";
+import {useEffect, useState} from "react"
 
   const columns = [
-  { field: 'col1', headerName: 'Lifetime', width: 150 },
-  { field: 'col2', headerName: 'Source', width: 150 },
-  { field: 'col3', headerName: 'Idea', width: 250 },
-  { field: 'col4', headerName: 'Description', width: 350 },
+  { field: 'createdAt', headerName: 'Lifetime', width: 150 },
+  { field: 'source', headerName: 'Source', width: 150 },
+  { field: 'ideaName', headerName: 'Idea', width: 250 },
+  { field: 'description', headerName: 'Description', width: 350 },
 ];
 
+async function getIdeas(){
+  const Ideas = Parse.Object.extend("Ideas")
+  const query = new Parse.Query(Ideas)
+  query.include("username")
+  return await query.find()
+}
+
 export default function Table() {
+const [ideas, setIdeas] = useState();
+
+  useEffect(() => {
+    getIdeas().then((ideas) => {
+        setIdeas(ideas)
+    })
+    
+  }, [])
+
+if (!ideas ) {
+  return <p>loading</p>
+}
+
+let rows = [];
+rows = ideas.map((row, index) => {
+  console.log(row.get("ideaSource").get("first_name"))
+  return ({
+    id: index,
+    createdAt: row.createdAt,
+    source: row.get("ideaSource").get("first_name"), //usernames,//row.attributes.ideaSource.id,//row.get("ideaSource").get("username"), //obj.attributes.ideasource.username,
+    ideaName: row.attributes.ideaName,
+    description: row.attributes.description,
+  });
+  
+});
+
     return (
       <div style={{ height: 500, width: '100%' }}>
           <div style={{ display: 'flex', height: '100%' }}>
