5023fac9512707311db62a972a401388383611ed
Daniel Bruhn Jensen <dbrj@itu.dk>
2021-11-23 17:38:50 +0100

    More Database work and updates to SelectFields

--- webserver/src/database/Users.js
+++ webserver/src/database/Users.js
@@ -4,4 +4,35 @@ export async function getUsers(){
     const Users = Parse.Object.extend("User")
     const query = new Parse.Query(Users)
     return await query.find()
+}
+
+export async function getEditors() {
+  const Users = Parse.Object.extend("Users");
+  const query = new Parse.Query(Users);
+  query.equalTo("isEditor", "True")
+
+  return await query.find();
+}
+
+export async function getPhotographers() {
+  const Users = Parse.Object.extend("Users");
+  const query = new Parse.Query(Users);
+  query.equalTo("isPhotographer", "True")
+
+  return await query.find();
+}
+
+export async function getUsersFromSection(section) {
+
+  if (section != "Foreign Affairs" || section != "Financial") {
+      console.log("Error: Section does not exist")
+      return new Error("Error: Section does not exist");
   }
+
+  else {
+      const Users = Parse.Object.extend("Users");
+      const query = new Parse.Query(Users);
+      query.equalTo("section", section)
+      return await query.find();
+  }
+}
