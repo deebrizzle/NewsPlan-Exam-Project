b7a5b646ee94c9bbcb91df51201a7a7f2294e41a
Celine Aaby Møller <celine@moellers.dk>
2021-11-24 11:41:23 +0100

    Added styles.js to usercontentarticle, moved code

--- webserver/src/components/UserContentArticle.js
+++ webserver/src/components/UserContentArticle.js
@@ -1,20 +1,43 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Typography, Grid } from "@mui/material";
+import { MyBox, Item } from "./UserContentArticle.styles";
 
 
-function UserContentArticle () {
-    return (
-        <Box sx={{flexGrow: 1}}>
-            <Typography variant="h6" component="h2" align="center"> Initials </Typography>
-            <Grid>
-                <Grid item xs={4}>Article</Grid>
-                <Grid item xs={4}>Article</Grid>
-                <Grid item xs={4}>Article</Grid>
-                <Grid item xs={4}>Article</Grid>
-                <Grid item xs={4}>Article</Grid>
-            </Grid>
-        </Box>
-    );
+function FormRow() {
+  return (
+    <React.Fragment>
+      <Grid item xs={4}>
+        <Item>Article</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Article</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Article</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Article</Item>
+      </Grid>
+      <Grid item xs={4}>
+        <Item>Article</Item>
+      </Grid>
+    </React.Fragment>
+  );
+}
+
+function UserContentArticle() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <Typography variant="h6" component="h2" align="center">
+        {" "}Initials{" "}
+      </Typography>
+      <Grid container spacing={5}>
+        <Grid item xs={4}>
+          <FormRow />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
 
 export default UserContentArticle;
