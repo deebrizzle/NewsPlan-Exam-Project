635b75f363794903c66d8717f595a3b96af51f91
Frida Helene Beck-Larsen <frib@itu.dk>
2021-12-01 15:37:22 +0100

    Renamed styled components

--- webserver/src/components/ArticleTable.js
+++ webserver/src/components/ArticleTable.js
@@ -7,7 +7,7 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import ErrorIcon from "@mui/icons-material/Error";
 import { ConvertDate } from "./ConvertDate";
-import { MyTableContainer,  MyTableLink} from "./Table.styles";
+import { StyledTableContainer, StyledTableLink, StyledTableCell } from "./Table.styles";
 import { StyledEditIcon } from "./Table.styles";
 
 //TODO: make styled component
@@ -19,50 +19,53 @@ function Notification(status) {
 }
 
 const ArticleTable = ({ articles }) => {
-
   //TODO: make button for accepting/denying
   //TODO: place the icons correctly and make them clickable
   //TODO: add color to colorfile
 
   return (
- 
     <TableContainer>
-         <MyTableContainer>
-      <Table stickyHeader={true} sx={{ minWidth: 200 }} aria-label="article table">
-        <TableHead>
-          <TableRow>
-            <TableCell style={{ background: "#EBF5FA" }}>Date</TableCell>
-            <TableCell style={{ background: "#EBF5FA" }}>Headline</TableCell>
-            <TableCell style={{ background: "#EBF5FA" }}>Writer</TableCell>
-            <TableCell style={{ background: "#EBF5FA" }}>Status</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {articles?.map((row) => (
-            <TableRow key={row.objectId}>
-              <TableCell>
-                {ConvertDate(String(row.get("publishDate")))}
-              </TableCell>
-              <TableCell>
-                <MyTableLink to={`/contentschedule/${row.id}`}>
-                  {row.get("headline")}{console.log(row.id)}
-                </MyTableLink>
-              </TableCell>
-              <TableCell>{row.get("responsible").get("username")}</TableCell>
-              <TableCell>
-                {row.get("status")}
-                <StyledEditIcon htmlColor="#415B68"
-                  fontSize="small">
-                </StyledEditIcon>
-                <Notification status={row.get("status")}></Notification>
-              </TableCell>
+      <StyledTableContainer>
+        <Table
+          stickyHeader={true}
+          sx={{ minWidth: 200 }}
+          aria-label="article table"
+        >
+          <TableHead>
+            <TableRow>
+              <StyledTableCell >Date</StyledTableCell>
+              <StyledTableCell >Headline</StyledTableCell>
+              <StyledTableCell >Writer</StyledTableCell>
+              <StyledTableCell >Status</StyledTableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      </MyTableContainer>
+          </TableHead>
+          <TableBody>
+            {articles?.map((row) => (
+              <TableRow key={row.objectId}>
+                <TableCell>
+                  {ConvertDate(String(row.get("publishDate")))}
+                </TableCell>
+                <TableCell>
+                  <StyledTableLink to={`/contentschedule/${row.id}`}>
+                    {row.get("headline")}
+                    {console.log(row.id)}
+                  </StyledTableLink>
+                </TableCell>
+                <TableCell>{row.get("responsible").get("username")}</TableCell>
+                <TableCell>
+                  {row.get("status")}
+                  <StyledEditIcon
+                    htmlColor="#415B68"
+                    fontSize="small"
+                  ></StyledEditIcon>
+                  <Notification status={row.get("status")}></Notification>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </StyledTableContainer>
     </TableContainer>
-  
   );
 };
 export default ArticleTable;
--- webserver/src/components/Table.styles.js
+++ webserver/src/components/Table.styles.js
@@ -1,16 +1,17 @@
 import styled from "styled-components";
-import { TableContainer} from "@mui/material";
+import { TableContainer, TableCell} from "@mui/material";
 import { Link } from "react-router-dom";
 import EditIcon from "@mui/icons-material/Edit";
+import colors from "./colors";
 
-export const MyTableContainer = styled(TableContainer)`
+export const StyledTableContainer = styled(TableContainer)`
     border-radius: 5px;
     height: 400px;
     width: 100%;
     overflow: initial;
 `
 
-export const MyTableLink = styled(Link)`
+export const StyledTableLink = styled(Link)`
 
     text-decoration: none;
     color: inherit;
@@ -22,4 +23,8 @@ export const StyledEditIcon = styled(EditIcon)`
     float: right;
     html-color: #415B68;
     font-size: small;
+`
+
+export const StyledTableCell = styled(TableCell)`
+    background: ${colors.P2BabyBlueLight}
 `
