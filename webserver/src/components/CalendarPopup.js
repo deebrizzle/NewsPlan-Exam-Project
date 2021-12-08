0d8928f999abffc4a47e54ca28c7daabea774b01
emte <emte@itu.dk>
2021-12-08 12:52:13 +0100

    Added CalendarPopUp back + delete IdeaTableFunc

--- webserver/src/components/CalendarPopup.js
+++ webserver/src/components/CalendarPopup.js
@@ -3,21 +3,24 @@ import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import DesktopDatePicker from "@mui/lab/DesktopDatePicker";
 import { useState } from "react";
+import React from "react";
+import { ModalContext } from "./ModalContext";
 
 export default function CalendarPopup({ handleCallbackDate, label }) {
+  const { date, setDate} = React.useContext(ModalContext);
   //TODO Ask Bjørn what he means by comment:
   //Line 8 and 9 will run every time state updates, even if the resulting const date is not used.
   //You are unlikely to run into any problems doing it like this, but as an exercise to understand React components and general clean code,
   //you might consider putting it all inside the initial value. No shame in that.
-
-  const currDate = new Date(2021, 10, 26, 0, 0, 0, 0);
-
-  const [date, setDate] = useState(currDate);
+  function formatDate(day) {
+    const formattedDay = new Date(day.setHours(0,0,0,0));
+    return formattedDay
+  }
 
   const handleChange = (newDate) => {
-    setDate(newDate);
-    handleCallbackDate(newDate);
-  };
+    setDate(formatDate(newDate));
+    handleCallbackDate(formatDate(newDate))
+  }
 
   return (
     <LocalizationProvider dateAdapter={AdapterDateFns}>
