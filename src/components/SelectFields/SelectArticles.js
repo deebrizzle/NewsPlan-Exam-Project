c70acf14badca7e6cb74b42f57df095ad30ba381
emte <emte@itu.dk>
2022-01-06 13:22:06 +0100

    get articles from idea

--- src/components/SelectFields/SelectArticles.js
+++ src/components/SelectFields/SelectArticles.js
@@ -1,12 +1,30 @@
 import BasicSelect from "./BasicSelect";
+import {getArticlesFromIdea} from "../../database/Articles"
+import React, { useContext, useEffect, useState } from "react";
+import { ModalContext } from "../ModalContext";
 
 export function SelectArticles() {
-    const articles = [];
+  const [articlesFromIdea, setArticlesFromIdea] = useState([]);
+  const {ideaId} = useContext(ModalContext);
+
+  useEffect(() => {
+      getArticlesFromIdea(ideaId).then((articles) => {
+        setArticlesFromIdea(articles);
+    });
+  }, []);
+
+  const articleObjects = articlesFromIdea.map((article) => {
+    return {
+      objectId: article.id,
+      name: article.get("headline"),
+    };
+  });
+
     return (
       <BasicSelect
         label="Articles"
-        value={articles}
-        arrayOfOptions={articles}
+        value={""}
+        arrayOfOptions={articleObjects}
       />
     );
   }
